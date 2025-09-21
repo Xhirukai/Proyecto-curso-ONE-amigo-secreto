3f85f6d4480f5bf1e94b56f407e29fe5d4e1e55f
Luis Daniel Rosas Miranda <luisdanielrosasmiranda@gmail.com>
2025-09-21 15:37:13 -0500

    Añadida al archivo JavaScript la funcion de actualizar lista de amigos y se inicia el desarrollo de la funcion de sorteo de amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -3,37 +3,58 @@ let listado_amigos = [];
 
 
 
-
-
-
-function agregar_texto_elemento(elemento, texto) {
-    let elemento_seleccionado = document.querySelector(elemento);
-    elemento_seleccionado.innerHTML = texto;
-    return;
-}
-
 function agregarAmigo() {
+
     let nombre_de_amigo = document.getElementById("amigo").value;
+
     if (nombre_de_amigo === "") {
 
         alert("El nombre del amigo no puede estar vacío");
-        agregar_texto_elemento("h2", "Digite el nombre de sus amigos");
 
     } else if (listado_amigos.includes(nombre_de_amigo)) {
 
         alert("El nombre del amigo ya está en la lista");
-        agregar_texto_elemento("h2", "Digite el nombre de sus amigos");
+        document.getElementById("amigo").value = "";
 
     } else {
 
         listado_amigos.push(nombre_de_amigo);
         document.getElementById("amigo").value = "";
-        
+
     }
     
-    document.getElementById("listaAmigos").innerHTML = listado_amigos.join(", ");
+    actualizarListaAmigos();
+
+    return;
+}
+
+function actualizarListaAmigos() {
+
+    let lista_a_mostrar = document.getElementById("listaAmigos");
+    lista_a_mostrar.innerHTML = ""; 
+
+    for (let posicion = 0; posicion < listado_amigos.length; posicion++) {
+
+        let amigo = listado_amigos[posicion];
+        let elemento_lista_a_mostrar = document.createElement("li");
+        elemento_lista_a_mostrar.innerText = amigo;
+        lista_a_mostrar.appendChild(elemento_lista_a_mostrar);
+
+    }
 }
 
 function sortearAmigo() {
+
+    let resultado = document.getElementById("resultado")
+
+    if (listado_amigos.length < 2) {
+        alert("Debe haber al menos dos amigos en la lista para realizar el sorteo.");
+        return;
+    } else {
+        let indice_aleatorio = Math.floor(Math.random*listado_amigos.length+1);
+        let amigo_seleccionado = listado_amigos[indice_aleatorio];
+        resultado.innerHTML = `Tu amigo secreto es ${amigo_seleccionado}`;
     
+        return;
+    }
 }
